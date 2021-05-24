184202f57c4ab2419718ccfabc1a545249bfde55
Henry Warui <Henry.Warui@MAIL.KAA>
2021-05-24 14:41:47 +0300

    Add birthdayCakeCandles solution

--- birthdayCakeCandles.js
+++ birthdayCakeCandles.js
@@ -1,15 +1,20 @@
 candles = [3,2,1,3,3,3];
 
-function birthdayCakeCandles(candles){
-    let len = candles.length;
-    if(len >=1 && len <= 50000){
-        let counts = {};
-        let max = Math.max(...candles);
+// function birthdayCakeCandles(candles){
+//     let len = candles.length;
+//     if(len >=1 && len <= 50000){
+//         let counts = {};
+//         let max = Math.max(...candles);
         
-        candles.forEach((x)=>{counts[x]= (counts[x] || 0) +1;})
-        counts[max];
-        console.log(`${counts[max]}`);
-    }
+//         candles.forEach((x)=>{counts[x]= (counts[x] || 0) +1;})
+//         counts[max];
+//         console.log(`${counts[max]}`);
+//     }
+// }
+
+function birthdayCakeCandles(candles){
+    const velas =  candles.filter(val => Math.max(...candles) === val);
+    return velas.length;
 }
 
-birthdayCakeCandles(candles);
+console.log(birthdayCakeCandles(candles));
